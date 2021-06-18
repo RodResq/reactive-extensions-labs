6e7e38f9ef143d65bf47ec3f8fbc2a7312aa26b2
rodrigo resque <rbresque@stafanni.com>
2021-06-18 00:23:49 -0300

    updt

--- src/app/core/form.component.ts
+++ src/app/core/form.component.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs';
 import { Component, DoCheck, Inject } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { Product } from "../model/product.model";
@@ -9,18 +10,22 @@ import { MODES, SharedState, SHARED_STATE } from "./sharedState.model";
     templateUrl: "form.component.html",
     styleUrls: ["form.component.css"]
 })
-export class FormComponent implements DoCheck {
+export class FormComponent {
     product: Product = new Product();
-    lastId: number;
+    editing: boolean = false;
 
     constructor(
         private model: Model,
-        @Inject(SHARED_STATE) private state: SharedState) {}
+        @Inject(SHARED_STATE) private stateEvents: Observable<SharedState>) {
+            stateEvents.subscribe((update) => {
+                this.product = new Product();
+                if(update.id != undefined) {
+                    Object.assign(this.product, this.model.getProduct(update.id));
+                }
+                this.editing = update.mode == MODES.EDIT;
+            });
+        }
 
-    get editing(): boolean {
-        return this.state.mode == MODES.EDIT;
-    }
-    
     submitForm(form: NgForm) {
         if (form.valid) {
             this.model.saveProduct(this.product);
@@ -33,14 +38,4 @@ export class FormComponent implements DoCheck {
         this.product = new Product();
     }
 
-    ngDoCheck(): void {
-        if(this.lastId != this.state.id) {
-            this.product = new Product();
-            if(this.state.mode == MODES.EDIT) {
-                Object.assign(this.product, this.model.getProduct(this.state.id));
-            }
-            this.lastId = this.state.id;
-        }
-        
-    }
 }
